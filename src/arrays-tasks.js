3817ca2090d58c2bf6c011ab3c4f2c53466370f4
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-22 17:51:44 +0400

    feat: solve isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -139,8 +139,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((el) => el.length === arr[0].length);
 }
 
 /**
