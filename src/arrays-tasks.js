5532038e96c30ff7ab0bc62621a1c18e5af86cbf
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-22 18:45:58 +0400

    feat: solve insertItem

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -170,8 +170,8 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
