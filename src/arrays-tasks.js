20d935ae570fae0a08f65edc50258c7e5f1cabae
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-22 22:28:21 +0400

    feat: solve getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -202,8 +202,9 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (!arr.length || n <= 0) return [];
+  return arr.slice(-n);
 }
 
 /**
