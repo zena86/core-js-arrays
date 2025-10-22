4c54f48b0296faff85a8759ce52b0ffbd65c19ef
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-22 17:45:44 +0400

    feat: solve getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -123,8 +123,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((acc, el) => acc + el, 0);
+  return +(sum / arr.length).toFixed(2);
 }
 
 /**
