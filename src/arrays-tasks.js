efd1bd37b65985b39b8df88b0befdcc630cee27f
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-26 19:44:39 +0400

    feat: solve getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -373,8 +373,8 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((acc, cur) => acc[cur], arr);
 }
 
 /**
