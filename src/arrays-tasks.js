5f8520ed864972f17a83ce76dd37e29442c21809
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-26 18:53:44 +0400

    feat: solve flattenArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -288,8 +288,8 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.flat(Infinity);
 }
 
 /**
