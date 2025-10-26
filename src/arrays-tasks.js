0f011aab5bdd6c3cca52a87e55ce84abd48457fb
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-26 18:36:28 +0400

    feat: solve distinct

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -250,8 +250,8 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return Array.from(new Set(arr));
 }
 
 /**
