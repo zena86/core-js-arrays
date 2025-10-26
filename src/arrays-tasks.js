a0997b4c412438c0c01230f3edc4f1781c2f6e99
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-10-26 19:30:22 +0400

    feat: solve generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -357,8 +357,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, i) => i * 2 + 1);
 }
 
 /**
