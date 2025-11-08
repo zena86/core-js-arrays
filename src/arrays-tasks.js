685fff9a134ba865b05b383b01074f7213e34b19
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-08 15:37:32 +0400

    feat: solve getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -428,8 +428,8 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.filter((el) => el % 2).map((el) => numbers.indexOf(el));
 }
 
 /**
